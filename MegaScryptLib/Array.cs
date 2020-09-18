df71388637848dabe4decbc6d2fa5fe82d0f658a
Trevor Alex <pascerveau@gmail.com>
2020-09-19 00:34:42 +0800

    array init with null

--- MegaScryptLib/Array.cs
+++ MegaScryptLib/Array.cs
@@ -13,7 +13,7 @@ namespace MegaScrypt {
         }
 
         public Array(IEnumerable<object> enumerable) {
-            m_list = enumerable.ToList();
+            m_list = enumerable is null ? null : enumerable.ToList();
             BindFunctions();
         }
         // TODO: Implement with the inherited interface: Stask.Get/Stack.Set
